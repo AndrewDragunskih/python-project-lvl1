ee3dc8e09f250bc3ee9fd49452869ba35bb5c047
andrew.dragunskih <andrew.dragunskih@yandex.ru>
2022-01-23 22:35:30 +0500

    step 4. add even games. fix linter errors

--- brain_games/greeting_user.py
+++ brain_games/greeting_user.py
@@ -1,11 +1,15 @@
 """greeting module."""
-
 import prompt
 
 
 def welcome_user():
-    """Greeting new user."""
+    """
+    Greet user, get user name, return user name - return str.
+
+    Returns:
+        str: user name
+    """
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
-    print('Hello,', name, sep = ' ', end = '!\n')
+    print('Hello,', name, sep=' ', end='!\n')
     return name
