b05330dfa0b2f533e06ed897939875c6418d34e2
andrew.dragunskih <andrew.dragunskih@yandex.ru>
2022-02-06 20:09:39 +0500

    fix print in welcome_user()

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -6,4 +6,4 @@ import prompt
 def welcome_user():
     """Greeting new user."""
     name = prompt.string('May I have your name? ')
-    print('Hello, ', name, '!')
+    print('Hello, {0}!'.format(name)
